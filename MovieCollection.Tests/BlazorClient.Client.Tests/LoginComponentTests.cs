f8a5a19697307862a9542095bf5bf4032fb65077
= <hjelte.jonatan@gmail.com>
2022-06-04 17:20:49 +0200

    Add LoginComponent

---
+++ MovieCollection.Tests/BlazorClient.Client.Tests/LoginComponentTests.cs
@@ -0,0 +1,63 @@
+﻿using Bunit;
+using MovieCollection.BlazorClient.Client.Components;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MovieCollection.Tests.BlazorClient.Client.Tests
+{
+    public class LoginComponentTests : TestContext
+    {
+        [Fact]
+        public void HasUserName()
+        {
+            var cut = RenderComponent<LoginComponent>();
+
+            Assert.Contains("USERNAME", cut.Markup.ToUpper());
+        }
+
+        [Fact]
+        public void HasPassword()
+        {
+            var cut = RenderComponent<LoginComponent>();
+
+            Assert.Contains("PASSWORD", cut.Markup.ToUpper());
+        }
+
+        [Fact]
+        public void HasLoginButton()
+        {
+            var cut = RenderComponent<LoginComponent>();
+
+            Assert.Contains("LOGIN", cut.Markup.ToUpper());
+        }
+
+        [Fact]
+        public void ClickLogin_UserNameEmpty_ShowsError()
+        {
+            var cut = RenderComponent<LoginComponent>();
+            var mudButton = cut.FindComponent<MudButton>();
+            var btn = mudButton.Find("button");
+            
+            btn.Click();
+
+            Assert.Contains("USERNAME IS REQUIRED", cut.Markup.ToUpper());
+        }
+
+        [Fact]
+        public void ClickLogin_PasswordEmpty_ShowsError()
+        {
+            var cut = RenderComponent<LoginComponent>();
+            var mudButton = cut.FindComponent<MudButton>();
+            var btn = mudButton.Find("button");
+
+            btn.Click();
+
+            Assert.Contains("PASSWORD IS REQUIRED", cut.Markup.ToUpper());
+        }
+    }
+}
--- MovieCollection.Tests/Services.Tests/UserServiceTests.cs
+++ MovieCollection.Tests/Services.Tests/UserServiceTests.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace MovieCollection.Tests.ServicesTests
+namespace MovieCollection.Tests.Services.Tests
 {
     public class UserServiceTests
     {
